8d0ece557bafd4ebdd757997aaf5d1c84b2641f7
Narayan Sajeev <narayansajeev1@gmail.com>
2022-07-28 14:09:56 -0400

    Implement Node, refactor code

--- app.js
+++ app.js
@@ -6,46 +6,70 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/websiteDB")
+
+const promptSchema = new mongoose.Schema({
+    title: String,
+    content: String
+})
+
+const Prompt = mongoose.model("Prompt", promptSchema)
+
+const promptTitles = ["What is something you like?",
+    "Describe one significant childhood memory.",
+    "Who are the most important people in your life?"]
 
 app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get("/index", (req, res) => {
-    res.redirect("/")
+app.get("/:page", (req, res) => {
+    const page = req.params.page
+
+    if (page === "help") {
+
+        const promptTitle = promptTitles[Math.floor(Math.random() * promptTitles.length)];
+
+        res.render(page, { promptTitle: promptTitle })
+
+    } else if (page === "prompts") {
+
+        const prompts = Prompt.find((err, savedPrompts) => {
+            res.render(page, {prompts: savedPrompts})
+        })
+
+    } else {
+        res.render(page, (err, html) => {
+            if (err) {
+                res.redirect("/")
+            } else {
+                res.send(html)
+            }
+        })
+
+    }
+
 })
 
-app.get("/about", (req, res) => {
-    res.render("about")
-})
+app.post("/help", (req, res) => {
+    
+    const promptTitle = req.body.promptTitle
+    const promptText = req.body.prompt
 
-app.get("/contact", (req, res) => {
-    res.render("contact")
-})
+    const newPrompt = new Prompt({
+        title: promptTitle,
+        content: promptText
+    })
 
-app.get("/signup", (req, res) => {
-    res.render("signup")
-})
+    newPrompt.save()
 
-app.get("/login", (req, res) => {
-    res.render("login")
-})
+    res.redirect("/help")
 
-app.get("/help", (req, res) => {
-    res.render("help")
-})
-
-app.get("/home", (req, res) => {
-    res.render("home")
-})
-
-app.get("/resources", (req, res) => {
-    res.render("resources")
 })
 
 app.listen(3000, () => {
     console.log("Server started successfully");
-  });
-  
+});
